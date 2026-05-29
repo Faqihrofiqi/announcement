b02e9f5733a4a9f69cb559ef61ac4b0cee804e1c
faqihrofiqi <21sa1076@mhs.amikompurwokerto.ac.id>
2026-05-29 21:33:13 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import TransitionView from './components/TransitionView';
 
 function App() {
   // CONFIGURATION: Set the announcement date here
-  const ANNOUNCEMENT_DATE = '2026-05-29T22:00:00';
+  const ANNOUNCEMENT_DATE = '2026-06-06T10:00:00';
   // TESTING MODE: Ubah ke 'true' untuk melewati hitung mundur, atau 'false' untuk mode produksi
-  const [isTimeReached, setIsTimeReached] = useState(false);
+  const [isTimeReached, setIsTimeReached] = useState(true);
   const [loggedInUser, setLoggedInUser] = useState(null);
   const [showTransition, setShowTransition] = useState(false);
   const [pendingUser, setPendingUser] = useState(null);
