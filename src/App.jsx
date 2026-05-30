d33d04cc011e37d601339367662d768b3b0442ba
Faqihrofiqi <134273835+Faqihrofiqi@users.noreply.github.com>
2026-05-30 08:18:46 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import TransitionView from './components/TransitionView';
 
 function App() {
   // CONFIGURATION: Set the announcement date here
-  const ANNOUNCEMENT_DATE = '2026-06-06T10:00:00';
+  const ANNOUNCEMENT_DATE = '2026-06-02T10:00:00';
   // TESTING MODE: Ubah ke 'true' untuk melewati hitung mundur, atau 'false' untuk mode produksi
   const [isTimeReached, setIsTimeReached] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState(null);
